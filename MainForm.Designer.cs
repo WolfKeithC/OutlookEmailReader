ecf0d509550bf8f59091ee2068966af476b54519
Keith Wolf <keith.c.wolf@abc.com>
2016-12-20 11:53:30 -0800

    first commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace MRMEmailReader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.lblAttach = new System.Windows.Forms.Label();
+            this.lstMsg = new System.Windows.Forms.ListView();
+            this.btnAccessEmail = new System.Windows.Forms.Button();
+            this.lblSubject = new System.Windows.Forms.Label();
+            this.lblAttachmentName = new System.Windows.Forms.Label();
+            this.lblSenderName = new System.Windows.Forms.Label();
+            this.lblSenderEmail = new System.Windows.Forms.Label();
+            this.lblCreationdate = new System.Windows.Forms.Label();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.cmbOutlookFolders = new System.Windows.Forms.ComboBox();
+            this.btnLoadMessages = new System.Windows.Forms.Button();
+            this.pbMessages = new System.Windows.Forms.ProgressBar();
+            this.lblEmailCount = new System.Windows.Forms.Label();
+            this.btnPizza = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(12, 13);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(246, 36);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "Load Folders";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(38, 401);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(140, 36);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Load Attachment";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // lblAttach
+            // 
+            this.lblAttach.AutoSize = true;
+            this.lblAttach.Location = new System.Drawing.Point(193, 411);
+            this.lblAttach.Name = "lblAttach";
+            this.lblAttach.Size = new System.Drawing.Size(139, 13);
+            this.lblAttach.TabIndex = 4;
+            this.lblAttach.Text = "No Attachment downloaded";
+            // 
+            // lstMsg
+            // 
+            this.lstMsg.Location = new System.Drawing.Point(12, 124);
+            this.lstMsg.Name = "lstMsg";
+            this.lstMsg.Size = new System.Drawing.Size(938, 196);
+            this.lstMsg.TabIndex = 5;
+            this.lstMsg.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnAccessEmail
+            // 
+            this.btnAccessEmail.Location = new System.Drawing.Point(12, 668);
+            this.btnAccessEmail.Name = "btnAccessEmail";
+            this.btnAccessEmail.Size = new System.Drawing.Size(140, 36);
+            this.btnAccessEmail.TabIndex = 6;
+            this.btnAccessEmail.Text = "Access Email";
+            this.btnAccessEmail.UseVisualStyleBackColor = true;
+            this.btnAccessEmail.Click += new System.EventHandler(this.btnAccessEmail_Click);
+            // 
+            // lblSubject
+            // 
+            this.lblSubject.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSubject.Location = new System.Drawing.Point(186, 575);
+            this.lblSubject.Name = "lblSubject";
+            this.lblSubject.Size = new System.Drawing.Size(370, 23);
+            this.lblSubject.TabIndex = 7;
+            this.lblSubject.Text = "label1";
+            this.lblSubject.Click += new System.EventHandler(this.lblSubject_Click);
+            // 
+            // lblAttachmentName
+            // 
+            this.lblAttachmentName.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAttachmentName.Location = new System.Drawing.Point(186, 609);
+            this.lblAttachmentName.Name = "lblAttachmentName";
+            this.lblAttachmentName.Size = new System.Drawing.Size(370, 23);
+            this.lblAttachmentName.TabIndex = 8;
+            this.lblAttachmentName.Text = "label1";
+            this.lblAttachmentName.Click += new System.EventHandler(this.lblAttachmentName_Click);
+            // 
+            // lblSenderName
+            // 
+            this.lblSenderName.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSenderName.Location = new System.Drawing.Point(186, 644);
+            this.lblSenderName.Name = "lblSenderName";
+            this.lblSenderName.Size = new System.Drawing.Size(370, 23);
+            this.lblSenderName.TabIndex = 9;
+            this.lblSenderName.Text = "label1";
+            this.lblSenderName.Click += new System.EventHandler(this.lblSenderName_Click);
+            // 
+            // lblSenderEmail
+            // 
+            this.lblSenderEmail.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSenderEmail.Location = new System.Drawing.Point(186, 678);
+            this.lblSenderEmail.Name = "lblSenderEmail";
+            this.lblSenderEmail.Size = new System.Drawing.Size(370, 23);
+            this.lblSenderEmail.TabIndex = 10;
+            this.lblSenderEmail.Text = "label1";
+            this.lblSenderEmail.Click += new System.EventHandler(this.lblSenderEmail_Click);
+            // 
+            // lblCreationdate
+            // 
+            this.lblCreationdate.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCreationdate.Location = new System.Drawing.Point(186, 712);
+            this.lblCreationdate.Name = "lblCreationdate";
+            this.lblCreationdate.Size = new System.Drawing.Size(370, 23);
+            this.lblCreationdate.TabIndex = 11;
+            this.lblCreationdate.Text = "label1";
+            this.lblCreationdate.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtBody
+            // 
+            this.txtBody.Location = new System.Drawing.Point(563, 575);
+            this.txtBody.Multiline = true;
+            this.txtBody.Name = "txtBody";
+            this.txtBody.ReadOnly = true;
+            this.txtBody.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtBody.Size = new System.Drawing.Size(323, 160);
+            this.txtBody.TabIndex = 12;
+            this.txtBody.WordWrap = false;
+            // 
+            // cmbOutlookFolders
+            // 
+            this.cmbOutlookFolders.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOutlookFolders.FormattingEnabled = true;
+            this.cmbOutlookFolders.Location = new System.Drawing.Point(12, 55);
+            this.cmbOutlookFolders.Name = "cmbOutlookFolders";
+            this.cmbOutlookFolders.Size = new System.Drawing.Size(246, 21);
+            this.cmbOutlookFolders.TabIndex = 13;
+            // 
+            // btnLoadMessages
+            // 
+            this.btnLoadMessages.Location = new System.Drawing.Point(12, 82);
+            this.btnLoadMessages.Name = "btnLoadMessages";
+            this.btnLoadMessages.Size = new System.Drawing.Size(246, 36);
+            this.btnLoadMessages.TabIndex = 14;
+            this.btnLoadMessages.Text = "Load Messages";
+            this.btnLoadMessages.UseVisualStyleBackColor = true;
+            this.btnLoadMessages.Click += new System.EventHandler(this.btnLoadMessages_Click);
+            // 
+            // pbMessages
+            // 
+            this.pbMessages.Location = new System.Drawing.Point(783, 401);
+            this.pbMessages.Name = "pbMessages";
+            this.pbMessages.Size = new System.Drawing.Size(100, 23);
+            this.pbMessages.TabIndex = 15;
+            // 
+            // lblEmailCount
+            // 
+            this.lblEmailCount.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblEmailCount.Location = new System.Drawing.Point(12, 323);
+            this.lblEmailCount.Name = "lblEmailCount";
+            this.lblEmailCount.Size = new System.Drawing.Size(938, 23);
+            this.lblEmailCount.TabIndex = 16;
+            this.lblEmailCount.Text = "label1";
+            // 
+            // btnPizza
+            // 
+            this.btnPizza.Location = new System.Drawing.Point(12, 763);
+            this.btnPizza.Name = "btnPizza";
+            this.btnPizza.Size = new System.Drawing.Size(140, 36);
+            this.btnPizza.TabIndex = 17;
+            this.btnPizza.Text = "Pizza";
+            this.btnPizza.UseVisualStyleBackColor = true;
+            this.btnPizza.Click += new System.EventHandler(this.btnPizza_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 363);
+            this.Controls.Add(this.btnPizza);
+            this.Controls.Add(this.lblEmailCount);
+            this.Controls.Add(this.pbMessages);
+            this.Controls.Add(this.btnLoadMessages);
+            this.Controls.Add(this.cmbOutlookFolders);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.lblCreationdate);
+            this.Controls.Add(this.lblSenderEmail);
+            this.Controls.Add(this.lblSenderName);
+            this.Controls.Add(this.lblAttachmentName);
+            this.Controls.Add(this.lblSubject);
+            this.Controls.Add(this.btnAccessEmail);
+            this.Controls.Add(this.lstMsg);
+            this.Controls.Add(this.lblAttach);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnRead);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MRM Email Reader";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Label lblAttach;
+        private System.Windows.Forms.ListView lstMsg;
+        private System.Windows.Forms.Button btnAccessEmail;
+        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.Label lblAttachmentName;
+        private System.Windows.Forms.Label lblSenderName;
+        private System.Windows.Forms.Label lblSenderEmail;
+        private System.Windows.Forms.Label lblCreationdate;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.ComboBox cmbOutlookFolders;
+        private System.Windows.Forms.Button btnLoadMessages;
+        private System.Windows.Forms.ProgressBar pbMessages;
+        private System.Windows.Forms.Label lblEmailCount;
+        private System.Windows.Forms.Button btnPizza;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,338 @@
+﻿using Microsoft.Exchange.WebServices.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MRMEmailReader
+{
+    public partial class MainForm : Form
+    {
+        ExchangeService exchange = null; 
+
+        public MainForm()
+        {
+            InitializeComponent();
+            lstMsg.Clear();
+            lstMsg.View = View.Details;
+            lstMsg.Columns.Add("Date", 150);
+            lstMsg.Columns.Add("From", 250);
+            lstMsg.Columns.Add("Subject", 400);
+            lstMsg.Columns.Add("Has Attachment", 50);
+            lstMsg.Columns.Add("Id", 100);
+            lstMsg.FullRowSelect = true;  
+        }
+
+        // Uses recursion to enumerate Outlook subfolders.
+        private void EnumerateFolders(Microsoft.Office.Interop.Outlook.MAPIFolder folder)
+        {
+            //cmbOutlookFolders.Items.Add(folder.Name);
+            Microsoft.Office.Interop.Outlook.Folders childFolders =
+                folder.Folders;
+            if (childFolders.Count > 0)
+            {
+                foreach (Microsoft.Office.Interop.Outlook.MAPIFolder childFolder in childFolders)
+                {
+                    try
+                    {
+                        // Write the folder path.
+                        Debug.WriteLine(childFolder.FolderPath);
+                        KeyValuePair<string, string> folderItem = new KeyValuePair<string, string>(childFolder.EntryID, childFolder.Name);// { }
+                        cmbOutlookFolders.Items.Add(folderItem);
+                        //cmbOutlookFolders.Items.Add(childFolder.Name);
+                        // Call EnumerateFolders using childFolder.
+                        EnumerateFolders(childFolder);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.Message);
+                    }
+                }
+
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Outlook.Application myApp = new Microsoft.Office.Interop.Outlook.Application();
+            Microsoft.Office.Interop.Outlook.NameSpace mapiNameSpace = myApp.GetNamespace("MAPI");
+            Microsoft.Office.Interop.Outlook.MAPIFolder myInbox = mapiNameSpace.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderInbox);
+
+            Microsoft.Office.Interop.Outlook.MAPIFolder oPublicFolder = myInbox.Parent;
+
+            cmbOutlookFolders.Items.Clear();
+
+            foreach (Microsoft.Office.Interop.Outlook.MAPIFolder folder in oPublicFolder.Folders)
+            {
+                //cmbOutlookFolders.Items.Add(folder.Name);
+                EnumerateFolders(folder);
+            }
+
+            cmbOutlookFolders.DisplayMember = "Value";
+            cmbOutlookFolders.ValueMember = "Key";
+
+            return;
+            /*
+            ConnectToExchangeServer();
+            TimeSpan ts = new TimeSpan(0, -1, 0, 0);
+            DateTime date = DateTime.Now.Add(ts);
+            SearchFilter.IsGreaterThanOrEqualTo filter = new SearchFilter.IsGreaterThanOrEqualTo(ItemSchema.DateTimeReceived, date);
+
+            if (exchange != null && exchange.Url != null)
+            {
+                FindItemsResults<Item> findResults = exchange.FindItems(WellKnownFolderName.Inbox, filter, new ItemView(50));
+
+                foreach (Item item in findResults)
+                {
+
+                    EmailMessage message = EmailMessage.Bind(exchange, item.Id);
+                    ListViewItem listitem = new ListViewItem(new[]   
+                    {  
+                        message.DateTimeReceived.ToString(), message.From.Name.ToString() + "(" + message.From.Address.ToString() + ")", message.Subject, ((message.HasAttachments) ? "Yes" : "No"), message.Id.ToString()  
+                    });
+                    lstMsg.Items.Add(listitem);
+                }
+                if (findResults.Items.Count <= 0)
+                {
+                    lstMsg.Items.Add("No Messages found!!");
+                }
+            }
+            else
+            {
+                lstMsg.Items.Add("Blank code objects!");
+            }
+            */
+        }
+        static bool RedirectionCallback(string url)
+        {
+            // Return true if the URL is an HTTPS URL.
+            return url.ToLower().StartsWith("https://");
+        }
+        /*
+        public void ConnectToExchangeServer()
+        {
+
+            lblMsg.Text = "Connecting to Exchange Server..";
+            lblMsg.Refresh();
+            try
+            {
+                exchange = new ExchangeService(ExchangeVersion.Exchange2013_SP1);
+                exchange.Credentials = new WebCredentials("WOLFK036", "*****", "SWNA");
+
+                string userEmailAddress = "keith.c.wolf@abc.com";
+                // Look up the user's EWS endpoint by using Autodiscover.
+                exchange.AutodiscoverUrl(userEmailAddress, RedirectionCallback);
+                
+                //exchange.AutodiscoverUrl("keith.c.wolf@abc.com");//, "https://autodiscover.disney.com/autodiscover/autodiscover.xml");
+
+                lblMsg.Text = "Connected to Exchange Server : " + exchange.Url.Host;
+                lblMsg.Refresh();
+
+            }
+            catch (Exception ex)
+            {
+                lblMsg.Text = "Error Connecting to Exchange Server!! " + ex.Message;
+                lblMsg.Refresh();
+            }
+        }
+        */
+
+        private void btnAccessEmail_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Outlook.Application myApp = new Microsoft.Office.Interop.Outlook.Application();
+            Microsoft.Office.Interop.Outlook.NameSpace mapiNameSpace = myApp.GetNamespace("MAPI");
+            Microsoft.Office.Interop.Outlook.MAPIFolder myInbox = mapiNameSpace.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderInbox);
+            if (myInbox.Items.Count > 0)
+            {
+                // Grab the Subject
+                lblSubject.Text = ((Microsoft.Office.Interop.Outlook.MailItem)myInbox.Items[1]).Subject;
+                //Grab the Attachment Name
+                if (((Microsoft.Office.Interop.Outlook.MailItem)myInbox.Items[1]).Attachments.Count > 0)
+                {
+                    lblAttachmentName.Text = ((Microsoft.Office.Interop.Outlook.MailItem)myInbox.Items[1]).Attachments[1].FileName;
+                }
+                else
+                {
+                    lblAttachmentName.Text = "No Attachment";
+                }
+                // Grab the Body
+                txtBody.Text = ((Microsoft.Office.Interop.Outlook.MailItem)myInbox.Items[1]).Body;
+                // Sender Name
+                lblSenderName.Text = ((Microsoft.Office.Interop.Outlook.MailItem)myInbox.Items[1]).SenderName;
+                // Sender Email
+                lblSenderEmail.Text = ((Microsoft.Office.Interop.Outlook.MailItem)myInbox.Items[1]).SenderEmailAddress;
+                // Creation date
+                lblCreationdate.Text = ((Microsoft.Office.Interop.Outlook.MailItem)myInbox.Items[1]).CreationTime.ToString();
+            }
+            else
+            {
+                MessageBox.Show("There are no emails in your Inbox.");
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)   
+        {  
+            if (exchange != null)   
+            {  
+                if (lstMsg.Items.Count > 0)   
+                {  
+                    ListViewItem item = lstMsg.SelectedItems[0];  
+  
+                    if (item != null)   
+                    {  
+                        string msgid = item.SubItems[4].Text.ToString();  
+                        EmailMessage message = EmailMessage.Bind(exchange, new ItemId(msgid));  
+                        if (message.HasAttachments && message.Attachments[0] is FileAttachment)   
+                        {  
+                            FileAttachment fileAttachment = message.Attachments[0] as FileAttachment;  
+                            //Change the below Path   
+                            fileAttachment.Load(@"C:\\Users\\Admin\\Documents\\Visual Studio 2012\\Projects\\ReadMailFromExchangeServer\\ReadMailFromExchangeServer\\Attachments\\" + fileAttachment.Name);  
+                            lblAttach.Text = "Attachment Downloaded : " + fileAttachment.Name;  
+                        }   
+                        else   
+                        {  
+                            MessageBox.Show("No Attachments found!!");  
+                        }  
+                    }   
+                    else   
+                    {  
+                        MessageBox.Show("Please select a Message!!");  
+                    }  
+                }   
+                else   
+                {  
+                    MessageBox.Show("Messages not loaded!!");  
+                }  
+                  
+            }   
+            else   
+            {  
+                MessageBox.Show("Not Connected to Mail Server!!");  
+            }  
+        }
+
+        private void btnLoadMessages_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Outlook.Application myApp = new Microsoft.Office.Interop.Outlook.Application();
+            Microsoft.Office.Interop.Outlook.NameSpace mapiNameSpace = myApp.GetNamespace("MAPI");
+
+            KeyValuePair<string, string> selectedItem = (KeyValuePair<string, string>)cmbOutlookFolders.SelectedItem;
+
+            Microsoft.Office.Interop.Outlook.MailItem mail01 = myApp.CreateItem(Microsoft.Office.Interop.Outlook.OlItemType.olMailItem) as Microsoft.Office.Interop.Outlook.MailItem;
+            Microsoft.Office.Interop.Outlook.MailItem mail02 = myApp.CreateItem(Microsoft.Office.Interop.Outlook.OlItemType.olMailItem) as Microsoft.Office.Interop.Outlook.MailItem;
+
+            mail01.Subject = "Move Deliverable Between TOWs Errors";
+            mail01.Body = "Hello Laura," + Environment.NewLine + Environment.NewLine + 
+                        "Attached are this week’s emails." + Environment.NewLine + Environment.NewLine + 
+                        "Thanks, Keith";
+
+            mail02.Subject = "WorkOrders with No Deliverable";
+            mail02.Body = "Hello Laura," + Environment.NewLine + Environment.NewLine +
+                        "Attached are this week’s emails." + Environment.NewLine + Environment.NewLine +
+                        "Thanks, Keith";
+
+            Microsoft.Office.Interop.Outlook.AddressEntry currentUser = myApp.Session.CurrentUser.AddressEntry;
+            Microsoft.Office.Interop.Outlook.ExchangeUser manager = currentUser.GetExchangeUser().GetExchangeUserManager();
+            // Add recipient using display name, alias, or smtp address
+            //mail.Recipients.Add(manager.PrimarySmtpAddress);
+            mail01.Recipients.ResolveAll();
+            mail02.Recipients.ResolveAll();
+
+            Microsoft.Office.Interop.Outlook.MAPIFolder myInbox = mapiNameSpace.GetFolderFromID(selectedItem.Key);
+            //cmbOutlookFolders.SelectedItem.ToString());// GetDefaultFolder(Microsoft.Office.Interop.Outlook.O.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderInbox);
+            int total = myInbox.Items.Count, count = 0, xytCount = 0, unread = 0;
+            pbMessages.Maximum = total;
+            if (total > 0)
+            {
+                foreach (Microsoft.Office.Interop.Outlook.MailItem item in myInbox.Items)
+                {
+                    count++;
+                    //pbMessages.Value = count;
+
+                    //if (!item.UnRead) continue;
+
+                    unread++;
+
+                    ListViewItem listitem = new ListViewItem(new[]   
+                    {  
+                        item.ReceivedTime.ToString(), item.SenderEmailAddress + "(" + item.Sender.Address.ToString() + ")", item.Subject, ((item.Attachments.Count > 0) ? "Yes" : "No"), item.EntryID.ToString()  
+                    });
+
+                    if (item.Body.Contains("Attempt to move a Deliverable between TOWs"))
+                    {
+                        listitem.BackColor = Color.Plum;
+                        xytCount++;
+
+                        mail01.Attachments.Add(item, Microsoft.Office.Interop.Outlook.OlAttachmentType.olByValue, Type.Missing, Type.Missing);
+                    }
+
+                    if (item.Body.Contains("Failure Message Deliverable with Id"))
+                    {
+                        listitem.BackColor = Color.GreenYellow;
+                        xytCount++;
+
+                        mail02.Attachments.Add(item, Microsoft.Office.Interop.Outlook.OlAttachmentType.olByValue, Type.Missing, Type.Missing);
+                    }
+
+                    lstMsg.Items.Add(listitem);
+                }
+
+                lblEmailCount.Text = string.Format("Total Emails: {0}, Emails Need Attention: {1}, Unread: {2} ", total, xytCount, unread);
+            }
+
+            mail01.Save();
+            mail02.Save();
+        }
+
+        private void OrderPizza()
+        {
+            Microsoft.Office.Interop.Outlook.Application myApp = new Microsoft.Office.Interop.Outlook.Application();
+            Microsoft.Office.Interop.Outlook.MailItem mail = myApp.CreateItem(Microsoft.Office.Interop.Outlook.OlItemType.olMailItem);
+            mail.VotingOptions = "Cheese; Mushroom; Sausage; Combo; Veg Combo;";
+            mail.Subject = "Pizza Order";
+            mail.Display(false);
+            mail.Save();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            lblEmailCount.Text = "Welcome";
+        }
+
+        private void lblAttachmentName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSubject_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSenderName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSenderEmail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPizza_Click(object sender, EventArgs e)
+        {
+            OrderPizza();
+        }
+    }
+}

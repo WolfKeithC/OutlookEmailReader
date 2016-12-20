7dfae6e7fc5f7f39c0c0a35fcb2ce75721e3773d
Keith Wolf <keith.c.wolf@abc.com>
2016-12-20 12:08:29 -0800

    fourth commit

--- MainForm.cs
+++ MainForm.cs
@@ -106,18 +106,13 @@ namespace MRMEmailReader
             Microsoft.Office.Interop.Outlook.MAPIFolder myInbox = mapiNameSpace.GetFolderFromID(selectedItem.Key);
             //cmbOutlookFolders.SelectedItem.ToString());// GetDefaultFolder(Microsoft.Office.Interop.Outlook.O.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderInbox);
             int total = myInbox.Items.Count, count = 0, xytCount = 0, unread = 0;
-            //pbMessages.Maximum = total;
+
             if (total > 0)
             {
                 foreach (Microsoft.Office.Interop.Outlook.MailItem item in myInbox.Items)
                 {
                     count++;
-                    //pbMessages.Value = count;
-
-                    //if (!item.UnRead) continue;
-
                     unread++;
-
                     ListViewItem listitem = new ListViewItem(new[]   
                     {  
                         item.ReceivedTime.ToString(), item.SenderEmailAddress + "(" + item.Sender.Address.ToString() + ")", item.Subject, ((item.Attachments.Count > 0) ? "Yes" : "No"), item.EntryID.ToString()  
